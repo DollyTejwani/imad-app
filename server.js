25a7f6a6e5fa1ffd2f66c65471314c45734a3a17
DollyTejwani <31108476+DollyTejwani@users.noreply.github.com>
2017-08-25 15:13:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,7 +94,7 @@ app.get('/', function (req, res) {
 });
 
 
-function createFunction(data){
+function createTemplate(data){
     var title= data.title;
     var date = data.date;
     var heading = data.heading;
@@ -152,25 +152,10 @@ function createFunction(data){
 
 
 app.get('/:articleName',function (req,res){
+    
+   var articleName = req.params.articleName;
 
-    res.send(createFunction(articles[articleName]));
-});
-
-app.get('/article-two',function (req,res){
-    res.sendFile(path.join(__dirname,'ui','article-two.html'));
-});
-
-
-app.get('/article-three',function (req,res){
-    res.sendFile(path.join(__dirname,'ui','article-one.html'));
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
